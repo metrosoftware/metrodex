7719527a3dcf9e1167ae15c32b995b4554dacd7a
metroceo <metrosoftware@zoho.com>
2018-06-18 12:34:51 +0700

    project name in sh files
    (cherry picked from commit 0bf886f)

--- win-release-package.sh
+++ win-release-package.sh
@@ -5,10 +5,10 @@ then
 	echo VERSION not defined
 	exit 1
 fi
-APPLICATION="metro-clone"
+APPLICATION="metro"
 PACKAGE=${APPLICATION}-client-${VERSION}
 echo PACKAGE="${PACKAGE}"
-CHANGELOG=metro-clone-client-${VERSION}.changelog.txt
+CHANGELOG=metro-client-${VERSION}.changelog.txt
 OBFUSCATE=$2
 
 FILES="changelogs conf html lib resource contrib logs"
